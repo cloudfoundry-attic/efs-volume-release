e9c80a2f226114f59fde56437d25cf43a8079bb8
Connor Braa <cbraa@pivotal.io>
2016-08-15 16:45:44 -0700

    bump efsbroker
    [finishes #127921921]
    
    Submodule src/code.cloudfoundry.org/efsbroker 6e98043..8f11253:
      > fixup: user can specify maxIO plan
    
    Signed-off-by: Luke Woydziak <luke.woydziak@emc.com>

--- scripts/generate-bosh-lite-manifest.sh
+++ scripts/generate-bosh-lite-manifest.sh
@@ -7,12 +7,23 @@ usage () {
     exit 1
 }
 
+if [ -z $AWS_ACCESS_KEY_ID ]; then
+  echo "FAILED: your AWS_ACCESS_KEY_ID is unset"
+  exit 1
+fi
+
+if [ -z $AWS_SECRET_ACCESS_KEY ]; then
+  echo "FAILED: your AWS_SECRET_ACCESS_KEY is unset"
+  exit 1
+fi
+
 templates=$(dirname $0)/../templates
 
 cat > ${PWD}/director-uuid.yml << EOF
 ---
-director_uuid: $(bosh status --uuid)
+director_uuid: $(bosh -t "https://192.168.50.4:25555" status --uuid)
 EOF
+
 
 cat > ${PWD}/efsbroker-creds.yml << EOF
 ---
