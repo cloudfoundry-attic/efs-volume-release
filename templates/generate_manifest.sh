7dcb06a7b1d9427a92de56fc3c6324f0f2e4833d
Luke Woydziak <luke.woydziak@emc.com>
2016-09-14 12:01:13 -0700

    remove pora and inoperable bosh-lite stuff (#130303631)[https://www.pivotaltracker.com/story/show/130303631]

--- templates/generate_manifest.sh
+++ templates/generate_manifest.sh
@@ -4,39 +4,25 @@
 set -e -x
 
 usage () {
-    echo "Usage: generate_manifest.sh bosh-lite|aws cf-manifest director-stub bosh-target bosh-username bosh-password efs-props-stub"
+    echo "Usage: generate_manifest.sh cf-manifest director-stub bosh-target bosh-username bosh-password efs-props-stub"
     echo " * default"
     exit 1
 }
 
 templates=$(dirname $0)
 
-if [[  "$1" != "bosh-lite" && "$1" != "aws" || -z $3 ]]
+if [[  "$1" == "bosh-lite" || "$1" == "aws" || -z $1 ]]
   then
     usage
 fi
 
+MANIFEST_NAME=efsvolume-aws-manifest
 
-if [ "$1" == "bosh-lite" ]
-  then
-    MANIFEST_NAME=efs-boshlite-manifest
-
-    spiff merge ${templates}/efs-manifest-boshlite.yml \
-    $3 \
-    $7 \
-    > ${PWD}/$MANIFEST_NAME.yml
-fi
-
-if [ "$1" == "aws" ]
-  then
-    MANIFEST_NAME=efsvolume-aws-manifest
-
-    spiff merge ${templates}/efsvolume-manifest-aws.yml \
-    $2 \
-    $3 \
-    $7 \
-    ${templates}/toplevel-manifest-overrides.yml \
-    > $PWD/$MANIFEST_NAME.yml
-fi
+spiff merge ${templates}/efsvolume-manifest-aws.yml \
+$2 \
+$3 \
+$7 \
+${templates}/toplevel-manifest-overrides.yml \
+> $PWD/$MANIFEST_NAME.yml
 
 echo manifest written to $PWD/$MANIFEST_NAME.yml
