39b369f4c78a27e41561a225fcdebd6c5490b8d6
Dave Walter <dwalter@pivotal.io>
2018-08-23 15:19:38 -0700

    Convert build scripts over to use efs-volume-release-concourse-tasks resource
    [#159780293](https://www.pivotaltracker.com/story/show/159780293)

--- scripts/fly-exec-certs.sh
+++ scripts/fly-exec-certs.sh
@@ -3,4 +3,4 @@ set -e
 
 scripts_path=./$(dirname $0)
 
-fly -t persi execute -c $scripts_path/ci/run_driver_cert.build.yml -i efs-volume-release=/Users/pivotal/workspace/efs-volume-release --privileged
+fly -t persi execute -c $scripts_path/ci/run_driver_cert.build.yml -i efs-volume-release-concourse-tasks=/Users/pivotal/workspace/efs-volume-release -i efs-volume-release=/Users/pivotal/workspace/efs-volume-release --privileged
--- scripts/fly-exec-pats.sh
+++ scripts/fly-exec-pats.sh
@@ -5,4 +5,4 @@ scripts_path=./$(dirname $0)
 
 export CF_USERNAME=admin
 
-fly -t persi execute -c $scripts_path/ci/run_pats_efsvolume.build.yml -i efs-volume-release=/Users/pivotal/workspace/efs-volume-release --privileged
+fly -t persi execute -c $scripts_path/ci/run_pats_efsvolume.build.yml -i efs-volume-release-concourse-tasks=/Users/pivotal/workspace/efs-volume-release -i efs-volume-release=/Users/pivotal/workspace/efs-volume-release --privileged
